1144dee9a845fc7e9d5c336315f429a001d7a1e6
Frost Wong <happyhackerwqc@foxmail.com>
2022-02-18 00:17:34 +0800

    init

---
+++ mclient.rb
@@ -0,0 +1,18 @@
+class Mclient < Formula
+  desc "Cli client for memcache server and other derivatives"
+  homepage "https://github.com/lovelock/mclient"
+  sha256 "c4250acb6921c13f8dccfc162d894bd7135a2979"
+  head "https://github.com/lovelock/mclient.git"
+
+  def install
+    # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
+    # is not set then it's forced to 10.4, which breaks compile on Mojave.
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
+    system "go build"
+    bin.install "mclient"
+  end
+
+  test do
+    system *%W[#{bin}/wrk2 -r 5 -c 1 -t 1 -d 1 https://example.com/]
+  end
+end
